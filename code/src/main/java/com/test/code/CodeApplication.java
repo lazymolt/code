634489fad0beba9407e2cc236c2d50888bc9b63b
molt <jwkoo88@gmail.com>
2019-08-16 20:04:18 +0900

    master change code

--- code/src/main/java/com/test/code/CodeApplication.java
+++ code/src/main/java/com/test/code/CodeApplication.java
@@ -8,6 +8,7 @@ public class CodeApplication {
 
 	public static void main(String[] args) {
 		System.out.println("11111");
+		System.out.println("master");
 		SpringApplication.run(CodeApplication.class, args);
 	}
 }
