64fd3a48362f74ae267edd9a5ad367ff3f0f133a
molt <jwkoo88@gmail.com>
2019-08-16 19:59:32 +0900

    2nd commit

--- code/src/main/java/com/test/code/CodeApplication.java
+++ code/src/main/java/com/test/code/CodeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("11111");
 		SpringApplication.run(CodeApplication.class, args);
 	}
 }
