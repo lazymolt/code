508c375f5f8eec6f60ff348c66dd1aa96c206790
molt <jwkoo88@gmail.com>
2019-08-16 20:05:13 +0900

    hotfix branch change code

--- code/src/main/java/com/test/code/CodeApplication.java
+++ code/src/main/java/com/test/code/CodeApplication.java
@@ -8,6 +8,7 @@ public class CodeApplication {
 
 	public static void main(String[] args) {
 		System.out.println("11111");
+		System.out.println("hotfix branch code change");
 		SpringApplication.run(CodeApplication.class, args);
 	}
 }
