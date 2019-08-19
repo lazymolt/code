127d5103fe2c0d8361b3e9c6278dd8565bff5326
molt <jwkoo88@gmail.com>
2019-08-19 22:17:43 +0900

    new code

---
+++ code/src/main/java/com/test/code/events/Event.java
@@ -0,0 +1,41 @@
+package com.test.code.events;
+
+import java.time.LocalDateTime;
+
+//@Builder @AllArgsConstructor @NoArgsConstructor
+//@Getter @Setter @EqualsAndHashCode(of ="id")
+//@Entity
+public class Event {
+
+//    @Id
+//    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location;
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+//    @Enumerated(EnumType.STRING) @Builder.Default
+//    private EventStatus eventStatus = EventStatus.DRAFT;
+
+    public void update() {
+        if(this.basePrice ==0 && this.maxPrice == 0) {
+            this.free = true;
+        } else {
+            this.free = false;
+        }
+
+        if("".equals(this.location) || this.location == null) {
+            this.offline = false;
+        } else {
+            this.offline = true;
+        }
+    }
+}
