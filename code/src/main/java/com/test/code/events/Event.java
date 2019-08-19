ef488b69747170ff2c8a828e5817eacd7ab622c2
lazymolt <40542515+lazymolt@users.noreply.github.com>
2019-08-19 22:37:35 +0900

    Update Event.java
    modify dto

--- code/src/main/java/com/test/code/events/Event.java
+++ code/src/main/java/com/test/code/events/Event.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 //@Getter @Setter @EqualsAndHashCode(of ="id")
 //@Entity
 public class Event {
-
+// modify Dto
 //    @Id
 //    @GeneratedValue
     private Integer id;
